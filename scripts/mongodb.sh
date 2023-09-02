3d6c246ee40123775d5d3572ff8b4d502b2606d4
CC11001100 <CC11001100@qq.com>
2023-09-03 01:27:41 +0800

    fix: scripts中环境变量修改为连接到本地

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -4,4 +4,4 @@
 docker run -d -p 27017:27017 --name storage-lock-mongodb -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=UeGqAm8CxYGldMDLoNNt mongo
 
 # 设置单元测试需要的环境变量
-export STORAGE_LOCK_MONGO_URI="mongodb://root:UeGqAm8CxYGldMDLoNNt@192.168.128.206:27017/?connectTimeoutMS=300000"
+export STORAGE_LOCK_MONGO_URI="mongodb://root:UeGqAm8CxYGldMDLoNNt@127.0.0.1:27017/?connectTimeoutMS=300000"
