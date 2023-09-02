48fee7f9d055e2b2cd4adfe6a3e57539cd3cc8e1
CC11001100 <CC11001100@qq.com>
2023-09-03 01:27:51 +0800

    feat: 为错误类型加注释

--- errors.go
+++ errors.go
@@ -3,7 +3,13 @@ package mongodb_storage
 import "fmt"
 
 var (
-	ErrDatabaseNameEmpty    = fmt.Errorf("DatabaseName can not empty")
-	ErrCollectionNameEmpty  = fmt.Errorf("CollectionName can not empty")
+
+	// ErrDatabaseNameEmpty 参数中数据库名字为空
+	ErrDatabaseNameEmpty = fmt.Errorf("DatabaseName can not empty")
+
+	// ErrCollectionNameEmpty 参数中集合名字为空
+	ErrCollectionNameEmpty = fmt.Errorf("CollectionName can not empty")
+
+	// ErrConnectionManagerNil 连接管理器没有指定
 	ErrConnectionManagerNil = fmt.Errorf("ConnectionManager can not nil")
 )
